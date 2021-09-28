e43a0fb0802083038085fc457c1f6f8d8ef577ab
isakwasser <wasser18@bk.ru>
2021-09-28 23:48:29 +0300

    change JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import {observer} from "./observer.js";
 setTimeout(function () {
     let el = document.querySelector('.nav__img').classList.add('nav__img-active');
     console.log(el);
@@ -6,3 +5,23 @@ setTimeout(function () {
 
 observer('.io');
 
+function observer(target) {
+    let options = { threshold: [0.4, 0.6] };
+
+    let observer = new IntersectionObserver(onTarget, options);
+    let elements = document.querySelectorAll(target);
+
+    elements.forEach(element => {
+        element.classList.add("io-hidden");
+        observer.observe(element);
+    });
+
+    function onTarget (entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                change.target.classList.add('io-show');
+            }
+            
+        });
+    }
+}
